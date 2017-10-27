89093c8f277e62a871f0edb08667618cb698909d
Will Charlton <willcharlton@exosite.com>
2017-10-27 11:03:33 -0500

    blah

--- co2_sensor.py
+++ co2_sensor.py
@@ -2,6 +2,29 @@
 
 from time import sleep
 import automationhat as hat
+import threading, Queue
 
-while True:
-    print("CO2: {}".format(hat.analog.one.read()))
+class PhotoDiode(threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.name = self.__class__.__name__
+        self.q_in = Queue.Queue()
+        self.q_out = Queue.Queue()
+
+    def _read(self):
+        return hat.analog.one.read()
+    def run(self):
+
+        init_val = self.read()
+        while True:
+            self.q_out.put(self.read())
+            sleep(0.05)
+def main():
+    photosensor = PhotoDiode()
+    photosensor.start()
+    while True:
+        print(photosensor.q_out.get())
+
+if __name__ == '__main__':
+    main()
